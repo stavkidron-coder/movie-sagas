c9eb3a1d633c43daf47ca716fbef98d732b86c3c
Stav Kidron <kidronstav@gmail.com>
2020-11-08 02:01:35 -0600

    bootstrap skeleton complete for add movie page

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import './AddMovie.css';
-import { Container, Button, Form, FormGroup, Label, Input, Jumbotron} from 'reactstrap';
+import { Container, Button, Form, FormGroup, Label, Input, Jumbotron, Col} from 'reactstrap';
 
 class AddMovie extends Component {
 
@@ -38,62 +38,73 @@ class AddMovie extends Component {
     }
 
     render() {
+
         return (
-            <Container>
+            <Container className="body">
                 <Jumbotron>
                     <Container>
                         <h1>Add a Movie!</h1>
-                        <Button outline color="primary" onClick={this.homeBtn}>Back to Movies</Button>
+                        <p>Enter in information to add a new movie to the database</p>
+                        <hr></hr>
+                        <Button color="primary" onClick={this.homeBtn}>Back to Movies</Button>
                     </Container>
                 </Jumbotron>
 
                 <Form>
+                        <FormGroup>
+                            <Col xs="4">
+                                <Label htmlFor="titleInput">Movie Title</Label>
+                                <Input id="titleInput" type="text" placeholder="Movie Title" onChange={(event) => this.handleChange(event, 'title')}/>
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup>
+                        <Col xs="4">
+                            <Label htmlFor="posterInput">Poster URL</Label>
+                            <Input id="posterInput" placeholder="Image URL" onChange={(event) => this.handleChange(event, 'poster')}/>
+                        </Col>
+                        </FormGroup>
+                    
                     <FormGroup>
-                        <Label htmlFor="titleInput">Movie Title</Label>
-                        <Input id="titleInput" type="text" placeholder="Movie Title" onChange={(event) => this.handleChange(event, 'title')}/>
+                        <Col xs="4">
+                            <Label htmlFor="descriptionInput">Movie Description</Label>
+                            <Input id="descriptionInput" type="textarea" placeholder="Movie Description" onChange={(event) => this.handleChange(event, 'description')}/>
+                        </Col>
                     </FormGroup>
 
                     <FormGroup>
-                        <Label htmlFor="posterInput">Poster URL</Label>
-                        <Input id="posterInput" placeholder="Image URL" onChange={(event) => this.handleChange(event, 'poster')}/>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label htmlFor="descriptionInput">Movie Description</Label>
-                        <Input id="descriptionInput" type="textarea" placeholder="Movie Description" onChange={(event) => this.handleChange(event, 'description')}/>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label htmlFor="genreInput">Movie Genre</Label>
-                        <Input type="select" name="genres" id="genreInput" onChange={(event) => this.handleChange(event, 'genre_id')}>
-                            <option value={0}>Select Genre</option>
-                            <option value={1}>Adventure</option>
-                            <option value={2}>Animated</option>
-                            <option value={3}>Biographical</option>
-                            <option value={4}>Comedy</option>
-                            <option value={5}>Disaster</option>
-                            <option value={6}>Drama</option>
-                            <option value={7}>Epic</option>
-                            <option value={8}>Fantasy</option>
-                            <option value={9}>Musical</option>
-                            <option value={10}>Romantic</option>
-                            <option value={11}>Science Fiction</option>
-                            <option value={12}>Space-Opera</option>
-                            <option value={13}>Superhero</option>
-                        </Input>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Button outline color="success" onClick={this.submitBtn}>Add Movie</Button>
+                        <Col xs="4">
+                            <Label htmlFor="genreInput">Movie Genre</Label>
+                            <Input type="select" name="genres" id="genreInput" onChange={(event) => this.handleChange(event, 'genre_id')}>
+                                <option value={0}>Select Genre</option>
+                                <option value={1}>Adventure</option>
+                                <option value={2}>Animated</option>
+                                <option value={3}>Biographical</option>
+                                <option value={4}>Comedy</option>
+                                <option value={5}>Disaster</option>
+                                <option value={6}>Drama</option>
+                                <option value={7}>Epic</option>
+                                <option value={8}>Fantasy</option>
+                                <option value={9}>Musical</option>
+                                <option value={10}>Romantic</option>
+                                <option value={11}>Science Fiction</option>
+                                <option value={12}>Space-Opera</option>
+                                <option value={13}>Superhero</option>
+                            </Input>
+                        </Col>
                     </FormGroup>
                     
+                    <FormGroup>
+                        <Col>
+                            <Button outline color="success" onClick={this.submitBtn}>Add Movie</Button>
+                        </Col>
+                    </FormGroup>
                 </Form>
+
             </Container>
         );
+        }
     }
-}
-
-
 
 const mapReduxStateToProps = reduxState => ({
     reduxState
