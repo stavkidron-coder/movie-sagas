da676d1db1b61f0a540395969fdfa142a23e35e8
Stav Kidron <kidronstav@gmail.com>
2020-11-08 01:10:49 -0600

    added addMovie.css, created form, added jumbotron

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Container, Button} from 'reactstrap';
+import './AddMovie.css';
+import { Container, Button, Form, FormGroup, Label, Input, Jumbotron} from 'reactstrap';
 
 class AddMovie extends Component {
 
@@ -30,40 +31,63 @@ class AddMovie extends Component {
     submitBtn = () => {
         console.log('movie to be submitted:', this.state.movie);
         this.props.dispatch({type: 'ADD_NEW_MOVIE', payload: this.state.movie});
+        // alert to let user know the movie was added
+        alert('Movie successfully added!');
+        // redirect back to movies page
+        this.props.history.push('/');
     }
 
     render() {
         return (
             <Container>
-                <div>
-                    <p>Add Movie Page</p>
-                    <Button outline color="primary" onClick={this.homeBtn}>Home</Button>
-                </div>
+                <Jumbotron>
+                    <Container>
+                        <h1>Add a Movie!</h1>
+                        <Button outline color="primary" onClick={this.homeBtn}>Back to Movies</Button>
+                    </Container>
+                </Jumbotron>
 
-                <div>
-                    <input type="text" placeholder="Movie Title" onChange={(event) => this.handleChange(event, 'title')}/>
-                    <input placeholder="Image URL" onChange={(event) => this.handleChange(event, 'poster')}/>
-                    <input placeholder="Movie Description" onChange={(event) => this.handleChange(event, 'description')}/>
+                <Form>
+                    <FormGroup>
+                        <Label htmlFor="titleInput">Movie Title</Label>
+                        <Input id="titleInput" type="text" placeholder="Movie Title" onChange={(event) => this.handleChange(event, 'title')}/>
+                    </FormGroup>
 
-                    <select name="genres" id="genres" onChange={(event) => this.handleChange(event, 'genre_id')}>
-                        <option value={0}>Select Genre</option>
-                        <option value={1}>Adventure</option>
-                        <option value={2}>Animated</option>
-                        <option value={3}>Biographical</option>
-                        <option value={4}>Comedy</option>
-                        <option value={5}>Disaster</option>
-                        <option value={6}>Drama</option>
-                        <option value={7}>Epic</option>
-                        <option value={8}>Fantasy</option>
-                        <option value={9}>Musical</option>
-                        <option value={10}>Romantic</option>
-                        <option value={11}>Science Fiction</option>
-                        <option value={12}>Space-Opera</option>
-                        <option value={13}>Superhero</option>
-                    </select>
+                    <FormGroup>
+                        <Label htmlFor="posterInput">Poster URL</Label>
+                        <Input id="posterInput" placeholder="Image URL" onChange={(event) => this.handleChange(event, 'poster')}/>
+                    </FormGroup>
 
-                    <Button outline color="success" onClick={this.submitBtn}>Add Movie</Button>
-                </div>
+                    <FormGroup>
+                        <Label htmlFor="descriptionInput">Movie Description</Label>
+                        <Input id="descriptionInput" type="textarea" placeholder="Movie Description" onChange={(event) => this.handleChange(event, 'description')}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label htmlFor="genreInput">Movie Genre</Label>
+                        <Input type="select" name="genres" id="genreInput" onChange={(event) => this.handleChange(event, 'genre_id')}>
+                            <option value={0}>Select Genre</option>
+                            <option value={1}>Adventure</option>
+                            <option value={2}>Animated</option>
+                            <option value={3}>Biographical</option>
+                            <option value={4}>Comedy</option>
+                            <option value={5}>Disaster</option>
+                            <option value={6}>Drama</option>
+                            <option value={7}>Epic</option>
+                            <option value={8}>Fantasy</option>
+                            <option value={9}>Musical</option>
+                            <option value={10}>Romantic</option>
+                            <option value={11}>Science Fiction</option>
+                            <option value={12}>Space-Opera</option>
+                            <option value={13}>Superhero</option>
+                        </Input>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Button outline color="success" onClick={this.submitBtn}>Add Movie</Button>
+                    </FormGroup>
+                    
+                </Form>
             </Container>
         );
     }
